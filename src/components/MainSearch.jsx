f4db1531337adf85a833e6e67b95e57e728ae14b
gretel-hans <adjeihansel@icloud.com>
2023-04-11 16:18:55 +0200

    first commit

---
+++ src/components/MainSearch.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { getFetch } from '../redux/actions'
+import Job from './Job'
+
+const MainSearch = () => {
+  const [query, setQuery] = useState('')
+  //const [jobs, setJobs] = useState([])
+  const jobs=useSelector((state)=>{
+    return state.joblist.list
+  })
+  const dispatch=useDispatch()
+
+  const navigate = useNavigate()
+
+ // const baseEndpoint = 'https://strive-benchmark.herokuapp.com/api/jobs?search='
+
+  const handleChange = (e) => {
+    setQuery(e.target.value)
+  }
+
+
+
+    const handleSubmit = async (e) => {
+    e.preventDefault()
+    dispatch(getFetch(query))
+    
+  } 
+
+
+
+  return (
+    <Container>
+      <Row>
+        <Col xs={10} className="mx-auto my-3">
+          <h1>Remote Jobs Search</h1>
+          <Button onClick={() => navigate('/favourites')}>Favourites</Button>
+        </Col>
+        <Col xs={10} className="mx-auto">
+          <Form onSubmit={handleSubmit}>
+            <Form.Control
+              type="search"
+              value={query}
+              onChange={handleChange}
+              placeholder="type and press Enter"
+            />
+          </Form>
+        </Col>
+        <Col xs={10} className="mx-auto mb-5">
+          {jobs.map((jobData) => (
+            <Job key={jobData._id} data={jobData} />
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default MainSearch
---
+++ src/redux/actions/index.js
@@ -0,0 +1,41 @@
+import { useDispatch } from "react-redux"
+
+export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE'
+export const REMOVE_FROM_FAVOURITE = 'REMOVE_FROM_FAVOURITE'
+export const SHOW_JOBS_LIST='SHOW_JOBS_LIST'
+
+
+export const addToFavorite = (jobs) => {
+    return {
+        type: ADD_TO_FAVOURITE,
+        payload: jobs
+    }
+}
+
+export const removeFromFavorite = (fav) => {
+    return {
+        type: REMOVE_FROM_FAVOURITE,
+        payload: fav
+    }
+}
+
+export const showJobsList = (jobs) => {
+    return {
+        type: SHOW_JOBS_LIST,
+        payload: jobs
+    }
+}
+
+export const getFetch=(query)=>{
+return async(dispatch,getState)=>{
+    try {
+        let response=await fetch(`https://strive-benchmark.herokuapp.com/api/jobs?search=${query}&limit=20`)
+        let jobslist= await response.json()
+        console.log('sono la fetch',jobslist.data)
+        dispatch(showJobsList(jobslist.data))
+    } catch (error) {
+        console.log(error)
+    }
+}
+}
+
